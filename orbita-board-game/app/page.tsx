b0268d2754eadfecf8bcdcfae0d11d0c09e5d9b7
tmdtjq211-tech <tmdtjq211@gmail.com>
2026-01-12 19:30:59 +0900

    Update page.tsx

--- orbita-board-game/app/page.tsx
+++ orbita-board-game/app/page.tsx
@@ -1,6 +1,6 @@
 // @/ 대신 상대 경로인 ../ 를 사용하여 경로 에러를 해결했습니다.
 // 중괄호 { } 를 제거하고 불러와야 에러가 나지 않습니다.
-import OrbitaGame from "../components/orbita-game"
+import OrbitaGame from "../components/orbita-game" // 이렇게 ../ 로 시작하게 수정
 
 export default function Home() {
   return (
