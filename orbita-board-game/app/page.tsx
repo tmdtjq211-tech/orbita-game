e0609898943b0459e7cc5ceb586c5feff6b938b7
tmdtjq211-tech <tmdtjq211@gmail.com>
2026-01-12 17:34:20 +0900

    Update page.tsx

--- orbita-board-game/app/page.tsx
+++ orbita-board-game/app/page.tsx
@@ -1,5 +1,10 @@
-import { OrbitaGame } from "@/components/orbita-game"
+// @/ 대신 상대 경로인 ../ 를 사용하여 경로 에러를 해결했습니다
+import OrbitaGame from "../components/orbita-game"
 
 export default function Home() {
-  return <OrbitaGame />
+  return (
+    <main className="min-h-screen bg-slate-950">
+      <OrbitaGame />
+    </main>
+  )
 }
