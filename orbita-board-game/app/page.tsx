547b8d3e7e66b3f23995af398c20de1c80fa6255
tmdtjq211-tech <tmdtjq211@gmail.com>
2026-01-12 17:41:01 +0900

    Update page.tsx

--- orbita-board-game/app/page.tsx
+++ orbita-board-game/app/page.tsx
@@ -1,4 +1,5 @@
-// @/ 대신 상대 경로인 ../ 를 사용하여 경로 에러를 해결했습니다
+// @/ 대신 상대 경로인 ../ 를 사용하여 경로 에러를 해결했습니다.
+// 중괄호 { } 를 제거하고 불러와야 에러가 나지 않습니다.
 import OrbitaGame from "../components/orbita-game"
 
 export default function Home() {
